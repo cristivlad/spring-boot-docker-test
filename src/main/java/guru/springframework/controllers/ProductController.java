5979a3b4381eab48f90cdad5db4c39e8e3d573b0
cristiii <cristian.vlad10@gmail.com>
2023-01-26 23:22:39 +0200

    added docker-compose file

--- src/main/java/guru/springframework/controllers/ProductController.java
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -1,5 +1,7 @@
 package guru.springframework.controllers;
 
+import guru.springframework.model.events.PageViewEvent;
+import guru.springframework.pageview.PageViewService;
 import guru.springframework.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,23 +9,35 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Date;
+import java.util.UUID;
+
 /**
  * Created by jt on 1/20/16.
  */
 @Controller
 public class ProductController {
 
-    private ProductService productService;
+    private final ProductService productService;
+    private final PageViewService pageViewService;
 
     @Autowired
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService, PageViewService pageViewService) {
         this.productService = productService;
+        this.pageViewService = pageViewService;
     }
 
     @RequestMapping("/product/{id}")
     public String getProductById(@PathVariable Integer id, Model model){
 
         model.addAttribute("product", productService.getProduct(id));
+
+        PageViewEvent pageViewEvent = new PageViewEvent();
+        pageViewEvent.setPageUrl("springframework.guru/product/" + id);
+        pageViewEvent.setPageViewDate(new Date());
+        pageViewEvent.setCorrelationId(UUID.randomUUID().toString());
+
+        pageViewService.sendPageViewEvent(pageViewEvent);
 
         return "product";
     }
